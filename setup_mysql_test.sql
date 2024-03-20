70bf0350f749ded0198c9b6b8b4334ade2380eb1
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-20 16:01:06 +0300

    mm

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Prepare a MySQL server
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev' @'localhost';
