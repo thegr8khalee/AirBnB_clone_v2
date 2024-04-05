c0348eea9d917eb6a0c4840debef9baad8d52aa5
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-04-05 21:55:50 +0300

    mm

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,61 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os.path
-from fabric.api import env, put, run
-from fabric.context_managers import cd
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-# Define remote hosts
 env.hosts = ["54.160.85.72", "35.175.132.106"]
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
-
     Args:
         archive_path (str): The path of the archive to distribute.
-
     Returns:
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if not os.path.isfile(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    # Extract file name and name without extension
-    file_name = os.path.basename(archive_path)
-    name = os.path.splitext(file_name)[0]
-
-    # Remote directory paths
-    remote_tmp_dir = "/tmp/"
-    remote_release_dir = "/data/web_static/releases/{}/".format(name)
-    remote_current_dir = "/data/web_static/current"
-
-    # Transfer the archive to the remote server's /tmp directory
-    put(archive_path, remote_tmp_dir)
-
-    # Create release directory
-    run("mkdir -p {}".format(remote_release_dir))
-
-    # Extract archive to release directory
-    with cd(remote_release_dir):
-        run("tar -xzf {} -C .".format(os.path.join(remote_tmp_dir, file_name)))
-
-    # Delete temporary archive file
-    run("rm {}".format(os.path.join(remote_tmp_dir, file_name)))
-
-    # Move contents from web_static directory to release directory
-    run("mv {}web_static/* {}".format(remote_release_dir, remote_release_dir))
-
-    # Remove now-empty web_static directory
-    run("rm -rf {}web_static".format(remote_release_dir))
-
-    # Update symbolic link
-    run("rm -rf {}".format(remote_current_dir))
-    run("ln -s {} {}".format(remote_release_dir, remote_current_dir))
-
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
+        return False
+    if (
+        run(
+            "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)
+        ).failed
+        is True
+    ):
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if (
+        run(
+            "mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name)
+        ).failed
+        is True
+    ):
+        return False
+    if (
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed
+        is True
+    ):
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if (
+        run(
+            "ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)
+        ).failed
+        is True
+    ):
+        return False
     return True
