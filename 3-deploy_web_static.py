4afe65575807ea19c31cb958e734875735484459
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-07-21 15:25:14 +0300

    mm

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+Fabric script that creates and distributes an archive to web servers
 
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
+Execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 """
 
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
+
 env.hosts = ['100.26.254.250', '54.236.45.4']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
 
 def do_pack():
-    """generates a tgz archive"""
+    """Generates a .tgz archive from the contents of the web_static folder."""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
+        if not isdir("versions"):
             local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
+        result = local("tar -cvzf {} web_static".format(file_name))
+        if result.failed:
+            return None
         return file_name
-    except:
+    except Exception as e:
+        print("An error occurred during packing:", e)
         return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to the web servers."""
+    if not exists(archive_path):
         return False
     try:
         file_n = archive_path.split("/")[-1]
@@ -40,13 +46,15 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        print("New version deployed!")
         return True
-    except:
+    except Exception as e:
+        print("An error occurred during deployment:", e)
         return False
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
+    """Creates and distributes an archive to web servers."""
     archive_path = do_pack()
     if archive_path is None:
         return False
