a04ff8ea096d3b6b15ef777a8610316457462353
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-07-28 21:12:01 +0300

    mm

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -81,7 +81,7 @@ def number_template(n):
     return render_template("5-number.html", number=n)
 
 
-@app.route("/number_odd_or_even/<int:n>")
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """
     Route handler for the /number_odd_or_even/<int:n> URL.
