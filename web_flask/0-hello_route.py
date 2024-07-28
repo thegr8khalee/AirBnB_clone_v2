0db82edc42ae8e7444d0113abd2c9b842ca2c9a9
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-07-28 15:08:14 +0300

    jhvkhg s

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#1/usr/bin/python3
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
