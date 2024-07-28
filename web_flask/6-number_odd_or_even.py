5c54464920d9609d8caa1b9a73448930608ff1ea
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-07-28 21:13:34 +0300

    mm

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -94,6 +94,5 @@ def number_odd_or_even(n):
     parity = "even" if n % 2 == 0 else "odd"
     return render_template("6-number_odd_or_even.html", number=n, parity=parity)
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
